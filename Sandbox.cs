0286d848287805e1c948cadb82e51141858abff5
Abraham Peláez Trujillo <abraham6541@hotmail.com>
2023-08-19 17:13:34 -0500

    Entrega de Parcial

--- Sandbox.cs
+++ Sandbox.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TestProject1
 {
@@ -6,8 +7,63 @@ namespace TestProject1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Console.WriteLine("Here you can write console prints to test your implementation outside the testing environment");
+            Stack<int> inputStack = new Stack<int>();
+            inputStack.Push(26);
+            inputStack.Push(3);
+            inputStack.Push(6);
+            inputStack.Push(5);
+
+            Stack<int> resultStack = TestMethods.GetNextGreaterValue(inputStack);
+
+            Console.WriteLine("Original Stack:");
+            foreach (int num in inputStack)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("Result Stack:");
+            foreach (int num in resultStack)
+            {
+                Console.WriteLine(num);
+            }
+
+            int[] sourceArr = { 2, 3, 5, 7, 10, 11, 15, 21 };
+            Dictionary<int, TestMethods.EValueType> valueDictionary = TestMethods.FillDictionaryFromSource(sourceArr);
+
+            Console.WriteLine("Value Dictionary:");
+            foreach (var kvp in valueDictionary)
+            {
+                Console.WriteLine($"Number: {kvp.Key}, Value Type: {kvp.Value}");
+            }
+            List<Ticket> ticketList = new List<Ticket>
+            {
+                new Ticket { Turn = 2, Request = TestMethods.ERequestType.Payment },
+                new Ticket { Turn = 1, Request = TestMethods.ERequestType.Subscription },
+                new Ticket { Turn = 3, Request = TestMethods.ERequestType.Cancellation },
+                new Ticket { Turn = 2, Request = TestMethods.ERequestType.Payments },
+                new Ticket { Turn = 1, Request = TestMethods.ERequestType.Subscriptions },
+            };
+
+            Queue<Ticket>[] ticketQueues = TestMethods.ClassifyTickets(ticketList);
+
+            Console.WriteLine("Classified Tickets:");
+            for (int i = 0; i < ticketQueues.Length; i++)
+            {
+                Console.WriteLine($"Queue {i + 1}:");
+                foreach (var ticket in ticketQueues[i])
+                {
+                    Console.WriteLine($"Turn: {ticket.Turn}, Request Type: {ticket.Request}");
+                }
+            }
+
+            Queue<Ticket> targetQueue = new Queue<Ticket>();
+            Ticket newTicket = new Ticket { Turn = 4, Request = TestMethods.ERequestType.Payments };
+            bool added = TestMethods.AddNewTicket(targetQueue, newTicket);
+
+            Console.WriteLine($"New Ticket Added: {added}");
+        }
+    }
+}
         }
     }
 }
--- TestMethods.cs
+++ TestMethods.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static TestProject1.Ticket;
 
 namespace TestProject1
 {
@@ -13,44 +16,135 @@ namespace TestProject1
             Prime
         }
 
-        internal static Stack<int> GetNextGreaterValue(Stack<int> sourceStack)
+        internal static Stack<int> GetNextGreaterValue(Stack<int> SourceStack)
         {
-            Stack<int> result = null;
+            Stack<int> result = new Stack<int>();
+            Stack<int> tempStack = new Stack<int>(SourceStack);
+            while (tempStack.Count > 0)
+            {
+                int currentElemento = tempStack.Pop();
+                bool foundMayor = false;
+                foreach (int elemento in tempStack)
+                {
+                    if (elemento > currentElemento)
+                    {
+                        result.Push(elemento);
+                        foundMayor = true;
+                        break;
+                    }
+                }
+                if (!foundMayor)
+                {
+                    result.Push(-1);
+                }
+            }
 
             return result;
         }
 
         internal static Dictionary<int, EValueType> FillDictionaryFromSource(int[] sourceArr)
         {
-            Dictionary<int, EValueType> result = null;
+            Dictionary<int, EValueType> result = new Dictionary<int, EValueType>();
+            foreach (int numero in sourceArr)
+            {
+                if (numero % 2 == 0)
+                {
+                    result[numero] = EValueType.Two;
+                }
+                else if (numero % 3 == 0)
+                {
+                    result[numero] = EValueType.Three;
+                }
+                else if (numero % 5 == 0)
+                {
+                    result[numero] = EValueType.Five;
+                }
+                else if (numero % 7 == 0)
+                {
+                    result[numero] = EValueType.Seven;
+                }
+                else if (IsPrime(numero))
+                {
+                    result[numero] = EValueType.Prime;
+                }
+
+            }
 
             return result;
+        }
+
+        private static bool IsPrime(int numero)
+        {
+            if (numero <= 1) return false;
+            if (numero == 2) return true;
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+
+            {
+                if (numero % i == 0) return false;
+            }
+            return true;
         }
 
         internal static int CountDictionaryRegistriesWithValueType(Dictionary<int, EValueType> sourceDict, EValueType type)
         {
-            return 0;
+            int count = 0;
+            foreach (KeyValuePair<int, EValueType> entry in sourceDict)
+            {
+                if (entry.Value == type)
+                {
+                    count++;
+                }
+
+            }
+            return count;
         }
 
         internal static Dictionary<int, EValueType> SortDictionaryRegistries(Dictionary<int, EValueType> sourceDict)
         {
-            Dictionary<int, EValueType> result = null;
-
-            return result;
+            var sortedDict = from entry in sourceDict orderby entry.Key descending select entry;
+            return sortedDict.ToDictionary(pair => pair.Key, pair => pair.Value);
         }
 
         internal static Queue<Ticket>[] ClassifyTickets(List<Ticket> sourceList)
         {
-            Queue<Ticket>[] result = null;
+            Queue<Ticket>[] result = new Queue<Ticket>[3];
+            result[0] = new Queue<Ticket>();
+            result[1] = new Queue<Ticket>();
+            result[2] = new Queue<Ticket>();
+
+            var sortedList = sourceList.OrderBy(ticket => ticket.Turn);
+            foreach (Ticket ticket in sortedList)
+            {
+                switch (ticket.Request)
+                {
+                    case ERequestType.Payment:
+                        result[0].Enqueue(ticket);
+                        break;
+                    case ERequestType.Subscription:
+                        result[1].Enqueue(ticket);
+                        break;
+                    case ERequestType.Cancellation:
+                        result[2].Enqueue(ticket);
+                        break;
+                }
+            }
 
             return result;
         }
 
         internal static bool AddNewTicket(Queue<Ticket> targetQueue, Ticket ticket)
         {
-            bool result = false;
-
-            return result;
-        }        
+            try
+            {
+                targetQueue.Enqueue(ticket);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
+
+        
